a7002c9b39de835880163d62caeed5c8afbac0a3
H3lel <moghadamartin@gmail.com>
2025-12-07 02:46:15 +0330

    server

---
+++ backend/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+from pathlib import Path
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+import uvicorn
+
+
+app = FastAPI()
+#api shits
+
+
+
+
+
+
+
+
+
+#spa setup
+build_dir = Path(__file__).parent.parent / "frontend" / "build"
+
+
+app.mount("/_app", StaticFiles(directory=build_dir / "_app"), name="_app")
+
+app.mount("/immutable", StaticFiles(directory=build_dir / "_app" / "immutable"), name="immutable")
+
+
+#  Fallback to index.html for all other routes (SPA routing)
+@app.get("/{full_path:path}")
+async def spa_fallback(full_path: str):
+    return FileResponse(build_dir / "index.html")
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=5000, reload=True)
